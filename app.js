c5ed3b61c8aa684819b8c04bbf1e6cd14b4509e1
arslan <arslanafzal321@gmail.com>
2020-03-01 02:57:48 +0500

    added error handling and logging package morgan

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders')
 
+app.use(morgan('dev'));
+
+//Routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Route Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
 
 module.exports =  app;
